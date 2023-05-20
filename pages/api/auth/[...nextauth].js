f7cffbe4bb60a1aebe4e8ad929be097790a42f6e
ido evergreen <dicksonevergreenido@gmail.com>
2023-05-20 03:17:41 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,13 @@ import NextAuth from "next-auth";
 // import GithubProvider from "next-auth/providers/github"
 // import Providers from "next-auth/providers";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
-
+import GoogleProvider from "next-auth/providers/google"
 import { db } from "../../../firebase";
 export default NextAuth({
   // Configure one or more authentication providers
 
   providers: [
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
